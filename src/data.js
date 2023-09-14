eca85a977c833ebc8940b602407acd8ac9c89646
Uduak <johnuduak035@gmail.com>
2023-09-14 13:15:40 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/378401501_1713052732441391_8183546598680820005_n.jpg?stp=cp6_dst-jpg_s600x600&_nc_cat=110&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeEcPNSazfIyqECH-xOy7Lsvl7p9iCk0CB6Xun2IKTQIHshRTMuPS1yNFPzQKmbb44pyWu5BpVZIWpPIYoHjwlZI&_nc_ohc=TDX4ULLGGm0AX8V_hyN&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfCBorFXqwWB79CWFhUzIgOW4rq-Wr6DvdBfwjNjhFmyjg&oe=6506F5A4",
+      title: "30% PRICE OFF ON EVERY ITEM",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://scontent.fphc2-1.fna.fbcdn.net/v/t39.30808-6/378166139_1713053069108024_653587231689709814_n.jpg?stp=cp6_dst-jpg_s600x600&_nc_cat=101&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeGkcnWHvSEAlhziAyNZiH4EFTUsaFVS-2AVNSxoVVL7YGWaFgLgT6kEU__nOaNruTjuH8r3ynFGa9zQIUIkTbBJ&_nc_ohc=JK70DVpb8I8AX9z3II7&_nc_zt=23&_nc_ht=scontent.fphc2-1.fna&cb_e2o_trans=q&oh=00_AfB9pO7CVfSSzNlO7twI7YSJjB0vqA4nM5CUxXoE-tiWAg&oe=65083A1F",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://scontent.fphc2-1.fna.fbcdn.net/v/t39.30808-6/378338997_1713053165774681_8601406109907120196_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeE0PPel1vHqfBoyFuDjwGEBQnP3RUbrB5ZCc_dFRusHljGo2u5k9xwqo0skGaK_ikNWi-h32C7ub65mAtmZGflZ&_nc_ohc=RshlUc62r7IAX88WT6o&_nc_zt=23&_nc_ht=scontent.fphc2-1.fna&cb_e2o_trans=q&oh=00_AfCcRp8WdkJ3K5T7zwqI0Mvg1t9EmiPOeIoSAv2Ob5TV1w&oe=65079596",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/378379515_1713052902441374_6863009630617386420_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeHvlynYfv2-WjzLZo1vplqFw8ilRPNRUibDyKVE81FSJmp1zG5Eeml2QJnGvkrqYsYOu4QomgcU3IYI-QOrc8Vp&_nc_ohc=6x4k514cJmEAX8OlIGz&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfDDO1qN9iVqgy34XfMjvWwrRksqKGGDvDaN71NJLKVU6w&oe=6508D53E",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://scontent.fphc2-1.fna.fbcdn.net/v/t39.30808-6/378156482_1713052995774698_1458142906023182768_n.jpg?stp=cp6_dst-jpg&_nc_cat=108&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeG058KUf57K48kd4Ofw4vFaiPABeLfl_jCI8AF4t-X-MCNp9cB78C8XoR_9jHnjwRkQrEejX2AwC5XaKhgqQyrc&_nc_ohc=8hB-AnFjPdwAX_b6Ltw&_nc_zt=23&_nc_ht=scontent.fphc2-1.fna&cb_e2o_trans=q&oh=00_AfA2x0iusMJcL4IKF4oII8k3m5Zxr5KkoQC6JMRgloJXZA&oe=650748D8",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/378277717_1713053085774689_8008447785215980314_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeHiYm7RkZwWqkdPUrbqIp0ycBGC_lvuaEFwEYL-W-5oQYDx-wQXuw9qDzyYOCfcnnSOb1egQUQyNG5Mjvz007Sc&_nc_ohc=QrDuhDto2QAAX9KVcS_&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfDmkT899mbAO6N1wdJs0lKy94imPgoxjm0nmup0L8rf7g&oe=6507F3D7",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/378197042_1713053135774684_1483416305916002763_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeG1H_SaPEE497_5pk5-X2XrwftV7NfmQBLB-1Xs1-ZAEjz5gFIjE5mgVtBruFkXpYmOVOr1aQz8JJurRh3uRXIe&_nc_ohc=DUbmYja1evAAX-uOFCd&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfAfuERoPHcq0fkxi2OwLM7RtR_b6FGKigpVCJF7BJ0dfg&oe=65072297",
+    },
+    {
+      id:2,
+      img:"https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/372825152_1708092792937385_8643138632177630385_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=104&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeE8CWsoyVRjz_99HuYEBa2p1cl40poZC-nVyXjSmhkL6QQE5jKZ_eB__JF2TOww8cVpgbrDUFp9xcKGJficAiPg&_nc_ohc=wTC7I2ZimN4AX-SqpHJ&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfBsKj5pnUKIl9Pgh9UAnDJ71se3oTl7JXZJcsU2E3tIMw&oe=65081838",
+    },
+    {
+      id:3,
+      img:"https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/363846261_1694712350942096_7125644625344316424_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=100&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeHO7QhZRbBcMm4BPjXhVrAFmJZj36WLMS-YlmPfpYsxL_8o-q932Z_R47GjA_xSlQ06rKsd_ZfgwzROoS9GaCoS&_nc_ohc=_mWmTftf84MAX-sgqSU&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfANgD-uklG8JzsyyWBZByf-VPVG52EJ1NVrt6YFM2AvFw&oe=65073B3F",
+    },
+    {
+      id:4,
+      img:"https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/363853046_1694712297608768_5086837053132578134_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=104&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeFm9FV4yByeTji96Y62O02Ca3CjoIvKsAVrcKOgi8qwBW08B29zShcYuinC8tynOBfuVBwQ0sU367jzrz7vKTkw&_nc_ohc=rfMzs3VfcPYAX-jEajP&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfD3YqjdSrJdZHwslX5smbPvdJUMnrX-7o9lVmX5EN8J7g&oe=6508753D",
+    },
+    {
+      id:5,
+      img:"https://scontent.fphc2-2.fna.fbcdn.net/v/t39.30808-6/364699227_1694712250942106_5846966400170396794_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=111&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeHXvn4lZ8Y_MVjudR0RlwswWTISXI6uSNxZMhJcjq5I3C_70OnYce7SZrC7iWk_BjORWhJVnrdVkhOGRdF1PDE1&_nc_ohc=9S6-ml827dUAX-upOX9&_nc_zt=23&_nc_ht=scontent.fphc2-2.fna&cb_e2o_trans=q&oh=00_AfCg7tV9ilKGHyeFCl5uPSdcr8bKWVNRZ1DLSKo2WdZa7g&oe=6507B754",
+    },
+    {
+      id:6,
+      img:"https://scontent.fphc2-1.fna.fbcdn.net/v/t39.30808-6/365653905_1694712170942114_8078049680562439962_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=102&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeFS7Q5qS9H8HNr4t3SiPIchMPvoxA6ROlQw--jEDpE6VI5Im8RUDpnt2743Ut0WZOD-F6t2-2zHs_aczslrX86O&_nc_ohc=mgOOpRRBhVMAX-VQUR6&_nc_zt=23&_nc_ht=scontent.fphc2-1.fna&cb_e2o_trans=q&oh=00_AfCjNFjlb9-xadJFhcAZiWLgnGXUWTtsKr3zBHBdhJXo-A&oe=650843E2",
+    },
+    {
+      id:7,
+      img:"https://scontent.fphc2-1.fna.fbcdn.net/v/t39.30808-6/364775196_1694712107608787_6362629311986624037_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=109&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeHTv1k21l2MH0-fVNa0msrRrSWpmJolzs2tJamYmiXOzTGxU7qy8ZnLq_hTNjDsz4Fh0-3PMOkIIiuqOPbtJqiA&_nc_ohc=QrJGKL41nskAX84B-th&_nc_zt=23&_nc_ht=scontent.fphc2-1.fna&cb_e2o_trans=q&oh=00_AfAZLVC_pPr3PX2-PrQTnElMMHu4w76G11llSlNLTuT7Wg&oe=6508C9C4",
+    },
+    {
+      id:8,
+      img:"https://scontent.fphc2-1.fna.fbcdn.net/v/t39.30808-6/359524034_1681273772285954_2074462283855850373_n.jpg?stp=c0.23.206.206a_cp6_dst-jpg_p206x206&_nc_cat=102&ccb=1-7&_nc_sid=0df3a7&_nc_eui2=AeFIwBxo-Kg5wcsyuXbPNA6gDgzJVNnc9fUODMlU2dz19eiKzgHrlrkjjAp4sCFmh7w_tRLh3GVqZ7nf4ZdyLxnj&_nc_ohc=2H5GR_PRk5cAX_J7p6e&_nc_oc=AQm5jx7jdx983deGLAzjCkWcMID5ZrHNSWhlGdutk1ES2R8sfOeETLIRv2MC7EsFN3c&_nc_zt=23&_nc_ht=scontent.fphc2-1.fna&cb_e2o_trans=q&oh=00_AfCc-FfyIFvT7jvlwEveoMcAyzxyG5RHRIw5uLMqFJZ6tw&oe=65082BE6",
+    },
+  ]
